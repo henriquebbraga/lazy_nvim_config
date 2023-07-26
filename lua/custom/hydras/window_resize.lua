78f5de1083fa00461f75fcdccdb85f061049022e
Henrique Bastos <hbastos@amazon.com>
2023-07-26 17:23:03 +0100

    add better window management

--- lua/custom/hydras/window_resize.lua
+++ lua/custom/hydras/window_resize.lua
@@ -1,43 +1,88 @@
-local hydra = require("hydra")
+local Hydra = require("hydra")
+local splits = require("smart-splits")
 
-local hint = [[
-  verical size
-  _k_/_e_ +
-  _j_/_u_ -
-  ^
-  horizontal size
-  _h_/_n_ +
-  _l_/_i_ -
-  ^
-  _q_ quit
+local cmd = require("hydra.keymap-util").cmd
+local pcmd = require("hydra.keymap-util").pcmd
+
+local window_hint = [[
+ ^^^^^^^^^^^^     Move      ^^    Size   ^^   ^^     Split
+ ^^^^^^^^^^^^-------------  ^^-----------^^   ^^---------------
+ ^ ^ _e_ ^ ^  ^ ^ _E_ ^ ^   ^  ^ _J_   ^   _s_: horizontally 
+ _n_ ^ ^ _i_  _N_ ^ ^ _I_   ^ _G_ ^  _Y_   _v_: vertically
+ ^ ^ _u_ ^ ^  ^ ^ _U_ ^ ^   ^  ^ _<_   ^   _q_, _c_: close
+ focus^^^^^^  window^^^^^^  ^_=_: equalize^   _z_: maximize
+ ^ ^ ^ ^ ^ ^  ^ ^ ^ ^ ^ ^   ^^ ^          ^   _o_: remain only
+
 ]]
 
-hydra({
-  name = "Window Resize",
-  hint = hint,
+Hydra({
+  name = "Windows",
+  hint = window_hint,
   config = {
-    color = "red",
     invoke_on_body = true,
     hint = {
       border = "rounded",
-      type = "window",
+      offset = -1,
     },
-    on_enter = function() end,
-    on_exit = function() end,
   },
-  mode = { "n" },
-  body = "<leader>ww",
+  mode = "n",
+  body = "<leader>wr",
   heads = {
-    { "j", ":resize -10<CR>", { silent = true, desc = "decrease verical size" } },
-    { "k", ":resize +10<CR>", { silent = true, desc = "increase verical size" } },
-    { "h", ":vertical resize +10<CR>", { silent = true, desc = "increase horizontal size" } },
-    { "l", ":vertical resize -10<CR>", { silent = true, desc = "dec horizontal size" } },
+    { "n", "<C-w>h" },
+    { "u", "<C-w>j" },
+    { "e", pcmd("wincmd k", "E11", "close") },
+    { "i", "<C-w>l" },
 
-    { "u", ":resize -10<CR>", { silent = true, desc = "decrease verical size" } },
-    { "e", ":resize +10<CR>", { silent = true, desc = "increase verical size" } },
-    { "n", ":vertical resize +10<CR>", { silent = true, desc = "increase horizontal size" } },
-    { "i", ":vertical resize -10<CR>", { silent = true, desc = "dec horizontal size" } },
+    { "N", cmd("WinShift left") },
+    { "U", cmd("WinShift down") },
+    { "E", cmd("WinShift up") },
+    { "I", cmd("WinShift right") },
 
-    { "q", nil, { exit = true, nowait = true, desc = "exit" } },
+    {
+      "G",
+      function()
+        splits.resize_left(2)
+      end,
+    },
+    {
+      "<",
+      function()
+        splits.resize_down(2)
+      end,
+    },
+    {
+      "J",
+      function()
+        splits.resize_up(2)
+      end,
+    },
+    {
+      "Y",
+      function()
+        splits.resize_right(2)
+      end,
+    },
+    { "=", cmd("WindowsEqualize"), { exit = true, desc = "equalize" } },
+
+    { "s", pcmd("split", "E36") },
+    { "<C-s>", pcmd("split", "E36"), { desc = false } },
+    { "v", pcmd("vsplit", "E36") },
+    { "<C-v>", pcmd("vsplit", "E36"), { desc = false } },
+
+    { "w", "<C-w>w", { exit = true, desc = false } },
+    { "<C-w>", "<C-w>w", { exit = true, desc = false } },
+
+    { "z", cmd("WindowsMaximize"), { exit = true, desc = "maximize" } },
+    { "<C-z>", cmd("WindowsMaximize"), { exit = true, desc = false } },
+
+    { "o", "<C-w>o", { exit = true, desc = "remain only" } },
+    { "<C-o>", "<C-w>o", { exit = true, desc = false } },
+
+    { "c", pcmd("close", "E444") },
+    { "q", pcmd("close", "E444"), { desc = "close window" } },
+    { "<C-c>", pcmd("close", "E444"), { desc = false } },
+    { "<C-q>", pcmd("close", "E444"), { desc = false } },
+
+    { "<Esc>", nil, { exit = true, desc = false } },
   },
 })
